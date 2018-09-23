560a55592bd0a961024d7f4a6e3b8356748f9b92
sung <khisungz@gmail.com>
2018-09-23 20:44:03 +0700

    Initial commit

---
+++ app/src/main/java/santoni/packag/com/kotlinapp4/MapsActivity.kt
@@ -0,0 +1,65 @@
+package santoni.packag.com.kotlinapp4
+
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private val USER_LOCATION_REQUEST_CODE = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        requestLocationPermission()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val montana = LatLng(46.877687, -109.369015)
+        mMap.addMarker(MarkerOptions().position(montana).title("Marker in Montana"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(montana))
+    }
+
+    // ask permission
+
+    private fun requestLocationPermission() {
+
+        if(Build.VERSION.SDK_INT>=23){
+
+            if(ActivityCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                ,USER_LOCATION_REQUEST_CODE)
+            }
+        }
+    }
+}
